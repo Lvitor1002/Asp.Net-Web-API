f032d3b64d403d437064a808ef5cee378a75fc1b
Luiz Vitor <luizpierinovitor@gmail.com>
2025-07-02 12:38:57 -0300

    up

--- obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59f760acaae8d5d8d2e5152f9a5e553d3497d74f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+029b57117cad7252fe848699e321586f7961841a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
